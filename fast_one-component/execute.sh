047ad08e0f4f7ba105b990b5befb66b8b210fe2d
Your Name <you@example.com>
2020-08-08 22:39:57 +0900

    修正

--- fast_one-component/execute.sh
+++
@@ -1,5 +0,0 @@
-jupyter nbconvert --to script '1_component.ipynb'
-jupyter nbconvert --to script 'analysis_1_component.ipynb'
-
-python3 1_component.py
-python3 analysis_1_component.py
--- fast_two-component/execute.sh
+++
@@ -1,5 +0,0 @@
-jupyter nbconvert --to script '2_component.ipynb'
-jupyter nbconvert --to script 'analysis_2_component.ipynb'
-
-python3 2_component.py
-python3 analysis_2_component.py
